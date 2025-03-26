3b529643bf70b17e207ab53779f5ba0e52857a4f
Jeff <ascaf27@gmail.com>
2025-03-26 17:00:21 +0800

    added get methods

--- index.js
+++ index.js
@@ -5,6 +5,22 @@ const app = express();
 
 app.use(bodyParser.json());
 
+const employees = [
+  { id: 1, name: 'Alice', position: 'Developer' },
+  { id: 2, name: 'Bob', position: 'HR Specialist' },
+];
+
+app.get('/employees',(req, res) => {
+  res.json(employees)
+})
+
+app.get('/employees/:id',(req, res) => {
+  //console.log(req.params.id)
+
+  res.json(employees.find(x => x.id === parseInt(req.params.id)).name)
+
+})
+
 app.listen(process.env.PORT || 3000, () => {
   console.log('Service is running');
 });
