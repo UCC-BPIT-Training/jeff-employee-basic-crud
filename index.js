2ac2fb2d5070ae917af7053653163f990ad8c74c
Jeff <ascaf27@gmail.com>
2025-03-26 20:39:16 +0800

    added post method

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser';
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false })); 
 app.use(bodyParser.json());
 
 const employees = [
@@ -10,16 +11,31 @@ const employees = [
   { id: 2, name: 'Bob', position: 'HR Specialist' },
 ];
 
-app.get('/employees',(req, res) => {
+app.get('/employees',(_, res) => {
   res.json(employees)
 })
 
 app.get('/employees/:id',(req, res) => {
-  //console.log(req.params.id)
-
-  res.json(employees.find(x => x.id === parseInt(req.params.id)).name)
-
+  // filter
+  const emp = employees.find(x => x.id === parseInt(req.params.id))
+  if(!emp){
+    return res.status(404).json({ message: 'Employee not found' });
+  }
+  res.status(200).json(emp);
 })
+
+app.post('/employees', (req, res) => {
+
+  const {name , position} = req.body;
+
+  if(!name || !position){
+    return res.status(404).json({ message: 'Details Incomplete' });
+  }
+  var max = Math.max(...employees.map(o=>o.id));
+  employees.push({id: max + 1, name: name, position: position})
+  res.status(200).json(employees);
+})
+
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('Service is running');
